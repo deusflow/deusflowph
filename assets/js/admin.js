c660a03095f955b95ef1ca87346ae0f51684c151
deusflow <deuswork@icloud.com>
2026-08-20 09:39:00 +0200

    feat: add viewport-fit=cover to meta tags and update asset versioning to v20260820-06

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,7 +5,7 @@ import {
   uploadToPhotosBucket,
   storagePathFromPublicUrl
 } from "./supabase-client.js";
-import { createStateMessage } from "./ui.js?v=20260820-05";
+import { createStateMessage } from "./ui.js?v=20260820-06";
 
 const state = {
   selectedAlbum: null,
